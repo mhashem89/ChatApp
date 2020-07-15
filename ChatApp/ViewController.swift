30588f0811f224fd0552146b7a378728c973f93b
Mohamed Hashem <Mohamed@A-PC.local>
2020-07-15 00:08:31 -0500

    getting started

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Mohamed Hashem on 7/15/20.
+//  Copyright © 2020 Mohamed Hashem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
